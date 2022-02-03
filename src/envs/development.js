cbc43696ccad626bba0a24acf14457618ace9561
Elisa Rossi <elisa.rossi@beije.it>
2022-02-03 17:34:06 +0100

    google analytics key for every environment

--- src/envs/development.js
+++ src/envs/development.js
@@ -1,6 +1,7 @@
 module.exports = {
   DEPLOY_ENV_NAME: 'DEVELOPMENT',
   API_REST_BE: 'https://virtserver.swaggerhub.com/beije/MGMT/1.0.0/',
+  GOOGLE_ANALITYCS_KEY: 'UA-215782182-1',
   ROUTING: {
     LOGIN_URL: '/login',
     BASE_URL: '/'
--- src/envs/prod.js
+++ src/envs/prod.js
@@ -1,5 +1,6 @@
 module.exports = {
   DEPLOY_ENV_NAME: 'PRODUCTION',
   API_REST_BE: '/',
-  BASE_URL: '/'
+  BASE_URL: '/',
+  GOOGLE_ANALITYCS_KEY: ''
 };
