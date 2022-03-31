1060e26cc089cb419db80840c1ecad630c95f48f
Federica Di Monaco <federica.dimonaco@beije.it>
2022-03-31 10:46:11 +0200

    markers gsap removed

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -68,8 +68,7 @@ const Home = (props) => {
     const t1 = gsap.timeline({
       scrollTrigger: {
         trigger: fifthSection,
-        start: 'top 75%',
-        markers: true
+        start: 'top 75%'
       },
     });
 
