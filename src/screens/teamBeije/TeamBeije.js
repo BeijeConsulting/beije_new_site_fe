8603971ecee160c8e2c5dd9d4dc33f9ce0804fd1
FedeRBeije <federico.reghini@beije.it>
2022-07-28 18:44:50 +0200

    deep scan

--- src/screens/teamBeije/TeamBeije.js
+++ src/screens/teamBeije/TeamBeije.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { isEmpty } from "lodash";
 import { Helmet } from "react-helmet";
