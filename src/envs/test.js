a6cc549aa3f552682e5caafc92f36876871f2383
Elisa Rossi <elisa.rossi@beije.it>
2022-09-01 18:27:52 +0200

    temporally fix

--- src/envs/test.js
+++ src/envs/test.js
@@ -1,5 +1,6 @@
 module.exports = {
   DEPLOY_ENV_NAME: 'TEST',
-  API_REST_BE: 'https://dev-mgmt.beije.it/',
+  API_REST_BE: 'https://mgmt-test-api.beije.it/',
+  // API_REST_BE: 'https://dev-mgmt.beije.it/',
   BASE_URL: '/'
 };
